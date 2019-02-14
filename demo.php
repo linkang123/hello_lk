170b3c60f8f47177b8eb12a19a7475c102ec063d
linknag123 <740074513@qq.com>
2019-02-14 14:06:09 +0800

    first first

---
+++ demo.php
@@ -0,0 +1,4 @@
+<?php
+require './vendor/autoload.php';
+use Hello_lk\Start_lk\tools;
+tools::Hello();
---
+++ src/Hello_lk/Start_lk/tools.php
@@ -0,0 +1,12 @@
+<?php
+namespace Hello_lk\Start_lk;
+class tools{
+
+public function __construct(){
+
+}
+public static function Hello(){
+	echo "Hello world";
+}
+
+}
